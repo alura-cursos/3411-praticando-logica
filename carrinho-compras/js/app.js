d0943b884d3b66775765f5a28d73ca348fbf786a
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-09-11 16:42:28 -0300

    2.3 - Calculando o valor do produto

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,9 +1,14 @@
 function adicionar() {
-    let nomeProduto = document.getElementById('produto');
+    let produto = document.getElementById('produto').value;
+    let nomeProduto = produto.split('-')[0];
+    let valorUnitario = produto.split('R$')[1];
     let quantidade = document.getElementById('quantidade');
+    let preco = quantidade.value * valorUnitario;
 
-    alert(nomeProduto.value);
+    alert(nomeProduto);
+    alert(valorUnitario);
     alert(quantidade.value);
+    alert(preco);
 }
 
 function limpar() {
