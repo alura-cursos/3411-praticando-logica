d31cdaa3f66bd98ac5e87274da0e7b1f58529608
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-09-11 17:01:33 -0300

    4.3 - Realizando compra para tipo pista

--- ingresso/js/app.js
+++ ingresso/js/app.js
@@ -1,7 +1,19 @@
 function comprar() {
     let tipo = document.getElementById('tipo-ingresso');
-    let qtd = document.getElementById('qtd');
+    let qtd = document.getElementById('qtd').value;
 
-    alert(tipo.value);
-    alert(qtd.value);
+    if (tipo.value == 'pista') {
+        comprarPista(qtd);
+    }
+}
+
+function comprarPista(qtd) {
+    let qtdPista = parseInt(document.getElementById('qtd-pista').textContent);
+    if (qtd > qtdPista) {
+        alert('Quantidade indisponível para tipo pista');
+    } else {
+        qtdPista = qtdPista - qtd;
+        document.getElementById('qtd-pista').textContent = qtdPista;
+        alert('Compra realizada com sucesso!');
+    }
 }
