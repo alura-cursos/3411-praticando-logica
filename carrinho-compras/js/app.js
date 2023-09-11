e2597f8496539998dc160dd753204787ef7029dd
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-09-11 16:48:47 -0300

    2.4 - Adicionando produto no carrinho

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -2,13 +2,13 @@ function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
     let valorUnitario = produto.split('R$')[1];
-    let quantidade = document.getElementById('quantidade');
-    let preco = quantidade.value * valorUnitario;
+    let quantidade = document.getElementById('quantidade').value;
+    let preco = quantidade * valorUnitario;
 
-    alert(nomeProduto);
-    alert(valorUnitario);
-    alert(quantidade.value);
-    alert(preco);
+    let carrinho = document.getElementById('lista-produtos');
+    carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
+    <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
+  </section>`;
 }
 
 function limpar() {
