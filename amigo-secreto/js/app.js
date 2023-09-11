9647efa13e6418e48444ffe9e3a59e3ef79756d9
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-09-11 17:23:20 -0300

    4.4 - Realizando o sorteio

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -17,6 +17,15 @@ function adicionar() {
 
 function sortear() {
     embaralhar(amigos);
+
+    let sorteio = document.getElementById('lista-sorteio');
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[0] + '<br/>';
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] +' --> ' +amigos[i + 1] + '<br/>';
+        }
+    }
 }
 
 function embaralhar(lista) {
