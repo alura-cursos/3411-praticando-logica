3e91ae188f0aa8864f4e18bb41316e0cdf8773ec
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-09-11 17:11:01 -0300

    5.2 - Adicionando amigo na lista

---
+++ amigo-secreto/js/app.js
@@ -0,0 +1,12 @@
+function adicionar() {
+    let amigo = document.getElementById('nome-amigo');
+    let lista = document.getElementById('lista-amigos');
+
+    if (lista.textContent == '') {
+        lista.textContent = amigo.value;
+    } else {
+        lista.textContent = lista.textContent + ', ' + amigo.value;
+    }
+
+    amigo.value = '';
+}
