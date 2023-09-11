a34864e2be3a289081048f4dc6883471d2c12d81
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-09-11 17:17:26 -0300

    5.3 - Embaralhando o array de amigos

--- amigo-secreto/js/app.js
+++ amigo-secreto/js/app.js
@@ -1,6 +1,10 @@
+let amigos = [];
+
 function adicionar() {
     let amigo = document.getElementById('nome-amigo');
     let lista = document.getElementById('lista-amigos');
+
+    amigos.push(amigo.value);
 
     if (lista.textContent == '') {
         lista.textContent = amigo.value;
@@ -9,4 +13,15 @@ function adicionar() {
     }
 
     amigo.value = '';
+}
+
+function sortear() {
+    embaralhar(amigos);
+}
+
+function embaralhar(lista) {
+    for (let indice = lista.length; indice; indice--) {
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+        [lista[indice - 1], lista[indiceAleatorio]] = [lista[indiceAleatorio], lista[indice - 1]];
+    }
 }
