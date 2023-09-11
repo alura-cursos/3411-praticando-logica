9525faae9af4c5c0999e85b35330d457897b576b
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-09-11 16:53:10 -0300

    2.5 - Calculando valor total do carrinho

--- carrinho-compras/js/app.js
+++ carrinho-compras/js/app.js
@@ -1,3 +1,6 @@
+let totalGeral;
+limpar();
+
 function adicionar() {
     let produto = document.getElementById('produto').value;
     let nomeProduto = produto.split('-')[0];
@@ -9,8 +12,15 @@ function adicionar() {
     carrinho.innerHTML = carrinho.innerHTML + `<section class="carrinho__produtos__produto">
     <span class="texto-azul">${quantidade}x</span> ${nomeProduto} <span class="texto-azul">R${preco}</span>
   </section>`;
+
+    totalGeral = totalGeral + preco;
+    let campoTotal = document.getElementById('valor-total');
+    campoTotal.textContent = `R$ ${totalGeral}`;
+    document.getElementById('quantidade').value = 0;
 }
 
 function limpar() {
-
+    totalGeral = 0;
+    document.getElementById('lista-produtos').innerHTML = '';
+    document.getElementById('valor-total').textContent = 'R$0';
 }
