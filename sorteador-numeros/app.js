f74a4c0a1af06d5115698a025a629c46aaafc849
Rodrigo Caneppele <r.caneppele@gmail.com>
2024-01-16 11:26:16 -0300

    1.2 - Projeto sorteador

---
+++ sorteador-numeros/app.js
@@ -0,0 +1,9 @@
+function sortear() {
+    let quantidade = document.getElementById('quantidade').value;
+    let de = document.getElementById('de').value;
+    let ate = document.getElementById('ate').value;
+
+    alert(`Quantidade: ${quantidade}`);
+    alert(`Do número: ${de}`);
+    alert(`Até o número: ${ate}`);
+}
