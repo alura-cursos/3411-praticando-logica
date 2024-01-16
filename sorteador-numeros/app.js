fa989e07624842eae6330c66169eb26f114e1dd0
Rodrigo Caneppele <r.caneppele@gmail.com>
2024-01-16 11:35:48 -0300

    1.4 - Exibindo numeros sorteados

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -5,13 +5,19 @@ function sortear() {
 
     let sorteados = [];
     let numero;
-    
+
     for (let i = 0; i < quantidade; i++) {
         numero = obterNumeroAleatorio(de, ate);
+
+        while (sorteados.includes(numero)) {
+            numero = obterNumeroAleatorio(de, ate);
+        }
+        
         sorteados.push(numero);
     }
     
-    alert(sorteados);
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
 }
 
 function obterNumeroAleatorio(min, max) {
